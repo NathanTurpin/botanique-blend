6a747743751f8c3a545ff0c969627ac447a8d0cc
nathan turpin <turpinn@dupont-restauration.fr>
2023-03-23 10:23:03 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+</script>
+
+<template>
+  <NavBar />
+  <header></header>
+  <main>
+    <section></section>
+  </main>
+  <footer></footer>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>cc</nav>
+</template>
+
+
+
+<style>
+nav {
+  color: var(--color-coffee);
+  font-size: 20rem;
+}
+</style>
