f402c22525f6c810899ca5fd4bc8ddd008f03af0
turpinNathan <nathan.turpin@free.fr>
2023-03-23 13:08:59 +0100

    navbar responsive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,52 +1,62 @@
 <script setup>
-import { ref, watchEffect } from 'vue'
+import { ref } from 'vue'
 import logoSrc from '@/assets/img/logo.svg'
-const showMenu = ref(false)
-const fullscreenMenu = ref(false)
-const screenWidth = ref(window.innerWidth)
+// const scrollPositon = ref(null)
+const mobile = ref(true)
+const mobileNav = ref(null)
+// const windowWidth = ref(window.innerWidth)
 
-watchEffect(() => {
-  if (screenWidth.value <= 1026 && showMenu.value) {
-    fullscreenMenu.value = true
-  } else {
-    fullscreenMenu.value = false
-  }
-})
-
-window.addEventListener('resize', () => {
-  screenWidth.value = window.innerWidth
-})
+const toggleMobileNav = () => {
+  console.log(mobileNav.value)
+  mobileNav.value = !mobileNav.value
+}
 </script>
 
 <template>
-  <nav class="nav">
-    <img :src="logoSrc" alt="altLogo" class="nav__img" />
+  <header :class="{ 'scrolled-nav': scrollPosition }">
+    <nav class="nav">
+      <div class="nav__branding">
+        <img :src="logoSrc" alt="altLogo" class="nav__logo" />
+      </div>
 
-    <ul
-      class="nav__ul"
-      :class="{
-        'nav__ul--show': showMenu,
-        'nav__ul--fullscreen': fullscreenMenu
-      }"
-    >
-      <li class="nav__li label label--small">La pépinière</li>
-      <li class="nav__li label label--small">La carte</li>
-      <li class="nav__li label label--small">Notre histoire</li>
-      <li class="nav__li label label--small">L’équipe</li>
-      <li class="nav__li label label--small">inspiration</li>
-    </ul>
+      <ul class="nav__navigation" v-show="!mobile">
+        <li class="nav__link label label--small">La pépinière</li>
+        <li class="nav__link label label--small">La carte</li>
+        <li class="nav__link label label--small">Notre histoire</li>
+        <li class="nav__link label label--small">L’équipe</li>
+        <li class="nav__link label label--small">inspiration</li>
+      </ul>
 
-    <div class="nav__toggle" @click="showMenu = !showMenu" v-if="screenWidth <= 1026">
-      <img
-        src="@/assets/img/nav/toggle.svg"
-        :class="{ 'nav__toggle-icon--open': showMenu }"
-        alt=""
-      />
-    </div>
-  </nav>
+      <div class="nav__icon">
+        <img v-show="mobile" @click="toggleMobileNav" src="@/assets/img/nav/toggle.svg" alt="" />
+      </div>
+
+      <transition name="mobile-nav">
+        <ul class="dropdown-nav" v-show="mobileNav">
+          <li class="dropdown-nav__link label label--small">La pépinière</li>
+          <li class="dropdown-nav__link label label--small">La carte</li>
+          <li class="dropdown-nav__link label label--small">Notre histoire</li>
+          <li class="dropdown-nav__link label label--small">L’équipe</li>
+          <li class="dropdown-nav__link label label--small">inspiration</li>
+        </ul>
+      </transition>
+
+      <transition name="mobile-nav-icon">
+        <div class="dropdown-nav__icon-close" v-show="mobileNav">
+          <img @click="toggleMobileNav" src="@/assets/img/nav/toggle.svg" alt="" />
+        </div>
+      </transition>
+    </nav>
+  </header>
 </template>
 
 <style scoped lang="scss">
+header {
+  position: fixed;
+  z-index: 99;
+  width: 100%;
+  transition: 0.5s ease all;
+}
 .nav {
   display: flex;
   flex-direction: row;
@@ -57,42 +67,57 @@ window.addEventListener('resize', () => {
   border-radius: 220px;
   padding: 1rem 2rem;
 
-  &__img {
+  &__branding {
+    display: flex;
+    align-items: center;
+  }
+
+  &__logo {
+    transition: 0.5 ease all;
     width: 5rem;
   }
-  &__toggle {
-    align-items: center;
-    justify-content: center;
-    width: 2rem;
-    height: 2rem;
-    cursor: pointer;
 
-    &-icon--open {
-      transform: rotate(90deg);
-    }
-  }
-  &__ul {
+  &__navigation {
     display: flex;
-    flex-direction: row;
+    justify-content: flex-end;
     gap: 2rem;
     list-style: none;
     align-items: center;
+    flex: 1;
+  }
 
-    &--show {
-      display: flex;
+  &__link {
+    color: var(--color-emeraude);
+  }
+
+  &__icon {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    cursor: pointer;
+    transition: 0.8s ease all;
+  }
+
+  .dropdown-nav {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: var(--color-base);
+    &__link {
+      color: wheat;
+      list-style: none;
     }
-
-    &--fullscreen {
-      background-color: rgba(245, 245, 245, 0.9);
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
+    &__icon-close {
+      background-color: white;
       z-index: 999;
     }
-  }
-  &__li {
-    color: var(--color-emeraude);
   }
 }
 </style>
