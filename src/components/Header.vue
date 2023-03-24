a6e236b421c393515437eceba0defe0453480492
turpinNathan <nathan.turpin@free.fr>
2023-03-24 16:27:14 +0100

    plus que responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,12 @@ import NavBar from './NavBar.vue'
     <NavBar />
     <div class="hero-banner">
       <div class="hero-banner__left">
-        <h1>Coffee Addict and Plant Lover, Botanic Blend est fait pour toi,</h1>
+        <img class="hero-banner__laur-top" src="@/assets/img/header/laur-top.svg" alt="" />
+        <img class="hero-banner__laur-bottom" src="@/assets/img/header/laur-bottom.svg" alt="" />
+        <h1 class="hero-banner__title">
+          Coffee Addict and Plant Lover, Botanic Blend est fait <br />
+          pour toi,
+        </h1>
 
         <div class="hero-banner__actions">
           <button>Découvrir les plantes</button>
@@ -17,8 +22,8 @@ import NavBar from './NavBar.vue'
       </div>
 
       <div class="hero-banner__right">
+        <div class="hero-banner__rec-fond"></div>
         <img class="hero-banner__left-small" src="@/assets/img/header/img-left-small.svg" alt="" />
-        <img class="hero-banner__rec-fond" src="@/assets/img/header/rec-fond.svg" alt="" />
         <img
           class="hero-banner__left-grande"
           src="@/assets/img/header/img-left-grande.svg"
@@ -40,32 +45,74 @@ import NavBar from './NavBar.vue'
   background-size: cover;
   background-position: center;
   background-color: var(--color-cream);
-  // background-color: red;
 }
 
 .hero-banner {
-  height: 90%;
+  height: 100%;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: 1fr;
-  grid-column-gap: 0px;
-  position: absolute;
-  grid-row-gap: 0px;
-  margin-top: 8vh;
+  grid-template-columns: minmax(0, 60vw) minmax(0, 40vw);
+
   &__left {
+    position: relative;
     grid-area: 1 / 1 / 2 / 2;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
   }
+
+  &__laur-top {
+    position: absolute;
+    top: 18%;
+    left: 0;
+  }
+  &__laur-bottom {
+    position: absolute;
+    top: 53%;
+    left: 0;
+  }
+  &__title {
+    width: 70%;
+    color: var(--color-emeraude);
+    z-index: 99;
+  }
+
+  &__actions {
+    display: flex;
+    flex-direction: row;
+    gap: 0.2rem;
+    width: 70%;
+
+    button {
+      padding: 12px 16px 12px 16px;
+      font-style: normal;
+      font-weight: 400;
+      font-size: 16px;
+      line-height: 20px;
+      color: var(--color-coffee);
+      border: 1px solid var(--color-coffee);
+      letter-spacing: 0.04em;
+      text-transform: uppercase;
+      &:first-child {
+        border: 1px solid var(--color-emeraude);
+        background-color: var(--color-emeraude);
+        color: var(--color-cream);
+      }
+    }
+  }
+
   &__right {
     grid-area: 1 / 2 / 2 / 3;
     position: relative;
   }
 
   &__rec-fond {
-    position: absolute;
-    top: 2%;
-    left: 0;
-    width: 100%;
+    background-image: url('@/assets/img/header/rec-fond.svg');
     height: 100%;
+    background-position: bottom;
+    background-size: cover;
+    background-repeat: no-repeat;
   }
 
   &__left-small {
@@ -87,8 +134,8 @@ import NavBar from './NavBar.vue'
 
   &__mid {
     position: absolute;
-    width: 19%;
-    top: 36%;
+    width: 23%;
+    top: 40%;
     left: 38%;
     z-index: 9;
   }
@@ -96,8 +143,8 @@ import NavBar from './NavBar.vue'
   &__img-right {
     position: absolute;
     top: 40%;
-    left: 52%;
-    width: 42%;
+    left: 50%;
+    width: 50%;
   }
 }
 </style>
