dd49ba7cef3357c2af58d4f64378ce904dba7727
nathan turpin <turpinn@dupont-restauration.fr>
2023-03-27 18:42:43 +0200

    leger bug a cause de la div des images

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
 import NavBar from './components/NavBar.vue'
+import FirstSection from './components/FirstSection.vue'
 </script>
 
 <template>
   <NavBar />
   <header></header>
   <main>
-    <section></section>
+    <FirstSection />
+    <!-- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur asperiores delectus illum
+    ratione, rerum doloribus distinctio eaque, ipsam dicta deleniti ducimus vel quo. Qui saepe
+    ratione ad, itaque provident dolorum veniam beatae corporis quidem soluta recusandae sit
+    cupiditate sapiente sequi placeat porro, earum cum! Sit sint sapiente cum debitis itaque
+    obcaecati minima nobis necessitatibus nulla laboriosam corrupti officia dolor, vel assumenda?
+    Magni porro animi numquam eveniet adipisci iure facere. In similique sint voluptatibus voluptate
+    sed soluta officiis vel nostrum consequatur eos doloremque incidunt tempora distinctio aperiam
+    est ipsa commodi id asperiores, dolore, labore veniam ducimus totam? Voluptate numquam expedita
+    distinctio. -->
   </main>
   <footer></footer>
 </template>
